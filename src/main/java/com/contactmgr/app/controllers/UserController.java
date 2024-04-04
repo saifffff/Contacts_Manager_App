0d16d7182c9b49f81ea85ceb3e4ec9a09203bbc4
saifffff <angryn1nj4@gmail.com>
2024-04-04 19:20:30 +0530

    added fully functional sidebar and add contact page
    backend code for add contact yet to be finished

--- src/main/java/com/contactmgr/app/controllers/UserController.java
+++ src/main/java/com/contactmgr/app/controllers/UserController.java
@@ -1,15 +1,34 @@
 package com.contactmgr.app.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.contactmgr.app.dao.UserRepository;
+import com.contactmgr.app.entities.Contact;
+import com.contactmgr.app.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserRepository userDao;
+	
+	@ModelAttribute
+	public void commonTask(Model model, Principal principal) {
+		// getting currently authenticated user 
+				String uEmail = principal.getName();
+				// using this email we can get user Object
+				User cUser = userDao.getUserByEmail(uEmail);
+				model.addAttribute("cUser",cUser);
+	}
 
 	@RequestMapping("/index")
-	public String dashboard(Model model) {
+	public String dashboard(Model model, Principal principal) {
 		model.addAttribute("title","Dashboard - Smart Contacts Manager");
 		return "normal/userDashboard";
 	}
@@ -18,6 +37,13 @@ public class UserController {
 	public String profile(Model model) {
 		model.addAttribute("title","Profile - Smart Contacts Manager");
 		return "normal/userProfile";
+	}
+	
+	@RequestMapping("/add-contact")
+	public String addContact(Model model) {
+		model.addAttribute("title","Add Contact - Smart Contacts Manager");
+		model.addAttribute("contact", new Contact());
+		return "normal/add-contact";
 	}
 	
 }
