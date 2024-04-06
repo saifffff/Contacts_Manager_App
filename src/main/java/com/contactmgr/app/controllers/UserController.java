f465aa5260cb69efe185ae93990e43b507c95a67
saifffff <angryn1nj4@gmail.com>
2024-04-06 18:50:37 +0530

    added image upload save and retrival logic
    add contact with image working fine now

--- src/main/java/com/contactmgr/app/controllers/UserController.java
+++ src/main/java/com/contactmgr/app/controllers/UserController.java
@@ -1,12 +1,23 @@
 package com.contactmgr.app.controllers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.contactmgr.app.dao.UserRepository;
 import com.contactmgr.app.entities.Contact;
@@ -17,6 +28,8 @@ import com.contactmgr.app.entities.User;
 public class UserController {
 	@Autowired
 	private UserRepository userDao;
+
+	
 	
 	@ModelAttribute
 	public void commonTask(Model model, Principal principal) {
@@ -43,6 +56,43 @@ public class UserController {
 	public String addContact(Model model) {
 		model.addAttribute("title","Add Contact - Smart Contacts Manager");
 		model.addAttribute("contact", new Contact());
+		return "normal/add-contact";
+	}
+	
+	@PostMapping("/process-contact")
+	public String contactHandler(@ModelAttribute Contact contact,
+			Principal principal,
+			Model model,
+			@RequestParam("profileImage") MultipartFile image
+			) {
+		/* we can save contact directly to the data base but we will require ContactRepository,
+		 * for now we will use user repository instead, so we need user instance
+		 */
+		User cUser = this.userDao.getUserByEmail(principal.getName());
+		cUser.getContacts().add(contact);
+		contact.setUser(cUser);
+		
+		try {
+			
+			if(!image.isEmpty()) {
+				/* we have to save this image in static/images, so first we need to fetch path
+				 * we will use ClassPathResource
+				 */
+				File res  = new ClassPathResource("static/images").getFile();
+				Path path = Paths.get(res.getAbsolutePath()+File.separator+image.getOriginalFilename());
+				Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				contact.setImageURL(image.getOriginalFilename());
+				System.out.println("image uploaded");
+			}
+			
+			User sUser = this.userDao.save(cUser);
+			System.out.println("saved : "+ contact.getName()+" for user : "+sUser.getName());
+			model.addAttribute("msg","Contact Added Successfully");
+		} catch (Exception e) {
+			model.addAttribute("err",e.getMessage());
+			e.printStackTrace();
+		}
+		
 		return "normal/add-contact";
 	}
 	
